2bffd68590680c98d05e021f4446f13f49dd36b8
AlfaBettaGamma <surmenko_konstantin@mail.ru>
2019-05-13 17:36:10 +0300

    update

--- lesson_2.py
+++ lesson_2.py
@@ -3,11 +3,14 @@
     factorial = 1
     emerald = 0
     nf = 0
+    if(N == 0 or N == 1):
+        emerald = 1
+        return emerald
     for i in range(2, N+1):
         factorial *= i
     nf = factorial
     while nf != 0:
         nf = nf//10
         s *= 10
-    emerald = factorial//(s / 10)
+    emerald = int(factorial//(s / 10))
     return (emerald)
---
+++ test.py
@@ -0,0 +1,36 @@
+import random
+class my_work:
+  def squirrel(self, N):
+    s = 1
+    factorial = 1
+    emerald = 0
+    nf = 0
+    if(N == 0 or N == 1):
+        emerald = 1
+        return emerald
+    for i in range(2, N+1):
+      factorial *= i
+      nf = factorial
+    while nf != 0:
+      nf = nf//10
+      s *= 10
+    emerald = int(factorial//(s / 10))
+    return (emerald)
+
+class my_test:
+  def test1(self):
+    test = my_work()
+    print(test.squirrel(0))
+
+  def test2(self):
+    test = my_work()
+    print(test.squirrel(1))
+
+  def test3(self):
+    test = my_work()
+    for i in range(10):
+      y = random.randint(0,10)
+      print(test.squirrel(y))
+
+test = my_test()
+test.test3()
