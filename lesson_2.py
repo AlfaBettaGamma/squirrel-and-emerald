138dd98f763c6f3e1bb215f16890c36007901d8b
AlfaBettaGamma <surmenko_konstantin@mail.ru>
2019-05-13 15:41:04 +0300

    Factorial

---
+++ lesson_2.py
@@ -0,0 +1,13 @@
+  def squirrel(self, N):
+    s = 1
+    factorial = 1
+    emerald = 0
+    nf = 0
+    for i in range(2, N+1):
+        factorial *= i
+    nf = factorial
+    while nf != 0:
+        nf = nf//10
+        s *= 10
+    emerald = factorial//(s / 10)
+    print (emerald)
